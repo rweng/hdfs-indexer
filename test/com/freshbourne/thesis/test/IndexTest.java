d2d42f518723cf50194203d7447799e42c840970
Robin Wenglewski <robin@wenglewski.de>
2010-12-08 16:28:06 +0100

    starting the testcase for index

---
+++ test/com/freshbourne/thesis/test/IndexTest.java
@@ -0,0 +1,19 @@
+package com.freshbourne.thesis.test;
+
+import com.freshbourne.thesis.Index;
+
+import junit.framework.TestCase;
+
+public class IndexTest extends TestCase {
+	private Index index;
+	
+	public void setUp(){
+		index = new Index(0);
+	}
+	
+	public void testAdd(){
+		String[] a = new String[0];
+		a[0] = "Alfred";
+		index.add(a, 0);
+	}
+}
