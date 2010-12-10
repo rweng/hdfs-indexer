9abb0e9ecb9c912f15926312db5671c70af88c73
Robin Wenglewski <robin@wenglewski.de>
2010-12-10 11:13:44 +0100

    automatic adjustment of tpch

--- scripts/install_tpch.sh
+++ scripts/install_tpch.sh
@@ -3,6 +3,19 @@
 mkdir tpch
 cd tpch
 
+CC=gcc
+DATABASE=DB2
+MACHINE=LINUX
+WORKLOAD=TPCH
+
+# download
 curl -LO http://www.tpc.org/tpch/spec/tpch_2_13_0.zip
 unzip tpch_*
 rm tpch_*.zip
+
+# compile
+sed "s/CC *= *$/CC=$CC/g" makefile.suite | \
+sed "s/DATABASE *= *$/DATABASE=$DATABASE/g" | \
+sed "s/MACHINE *= *$/MACHINE=$MACHINE/g" | \
+sed "s/WORKLOAD *= *$/WORKLOAD=$WORKLOAD/g" > makefile
+make && make install
