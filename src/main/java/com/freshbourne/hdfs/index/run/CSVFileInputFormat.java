d189d3d56c629450efc51af85f4010f0542c37bd
Robin Wenglewski <robin@wenglewski.de>
2010-12-11 12:23:13 +0100

    cleaned up FileInputFormat

--- src/main/java/com/freshbourne/hdfs/index/run/CSVFileInputFormat.java
+++ src/main/java/com/freshbourne/hdfs/index/run/CSVFileInputFormat.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.io.compress.CompressionCodecFactory;
 import org.apache.hadoop.mapreduce.InputSplit;
@@ -13,9 +12,6 @@ import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
-import edu.umd.cloud9.io.ArrayListWritableComparable;
-
 
 
 public class CSVFileInputFormat extends FileInputFormat<LongWritable, ArrayList<String>> {
