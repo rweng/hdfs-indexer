634b61c69df76b035727d71f130946b2991150a2
Robin Wenglewski <robin@wenglewski.de>
2011-09-29 09:33:03 +0200

    comments

--- src/main/java/com/freshbourne/hdfs/index/BTreeIndex.java
+++ src/main/java/com/freshbourne/hdfs/index/BTreeIndex.java
@@ -14,6 +14,50 @@ import java.util.AbstractMap;
 import java.util.Iterator;
 import java.util.Properties;
 
+/**
+ * This is the base class for all Indexes using the multimap btree.
+ *
+ * An instance can be created without any parameters to the constructor,
+ * but to ensure that it is working initialize must be called!
+ * The reason for not using the constructor are several:
+ * First, constructor inheritance isn't supported so for subclasses the constructor would have to be repeated.
+ * Second, in a constructor, this() has to be the first statement. This forces parsing to be done directly
+ * in the parameter which results in method calls like this:
+ *
+ * this(new File(inputSplitToFileSplit(genericSplit).getPath().toString()).getName(),
+ *               indexId,
+ *               new File(context.getConfiguration().get("indexSavePath")));
+ *
+ *
+ * 
+ * Also, to ensure that data is really written to the disk, close has to be called as specified by the
+ * Index interface.
+ *
+ * The instance gets a path in which all btrees are stored.
+ * The folder structure within this index-path is acording to the files of the HDFS.
+ *
+ * Example:
+ * If the index-path is /data/indexes, and an index is created over the second column of hdfs:///csvs/users.csv,
+ * the index files that are created are:
+ *
+ * /data/indexes/csvs/users.csv/properties.xml
+ * /data/indexes/csvs/users.csv/2_0
+ * /data/indexes/csvs/users.csv/2_10005
+ *
+ * With each btree, there are four information that have to be stored:
+ *
+ * - the hdfs file name and path
+ * - the identifier to be indexed (column, xml-path, ...)
+ * - the starting position in the hdfs file for the index
+ * - the end position in the hdfs file for the index
+ *
+ * The file name and path are stored in the structure in the index directory.
+ * The identifier and starting position could be stored directly in the file name.
+ * However, as it is unsure where the indexing will end at the time the index file is created,
+ * it is not possible to store the end position in the file name (assuming we dont want to rename).
+ * Thus, a properties file is required.
+ *
+ */
 public class BTreeIndex implements Index, Serializable {
 
     private static final long serialVersionUID = 1L;
