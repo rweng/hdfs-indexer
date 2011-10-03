3f8ebeaa7a3abe64d4623bbf9ed171d0db715c96
Robin Wenglewski <robin@wenglewski.de>
2011-10-03 08:53:06 +0200

    remove hdfs-indexer-test stuff to separate project

--- cookbooks/tpch/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "Robin Wenglewski"
-maintainer_email "robin@wenglewski.de"
-license          "All rights reserved"
-description      "Installs/Configures tpch"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-
-
-supports "ubuntu"
